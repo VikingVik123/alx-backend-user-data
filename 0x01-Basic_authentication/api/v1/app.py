4d5e970cbed8c2d0af34ceee1bea39bcd9519ff2
VikingVik123 <114689303+VikingVik123@users.noreply.github.com>
2024-09-03 16:26:50 +0100

    tried task5

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -45,23 +45,23 @@ def forbidden(error) -> str:
 
 
 @app.before_request
-def before_request_handler():
+def authenticate_user():
     """
-    Method to filter each request before processing it.
+    checks a user before processing a request.
     """
-    if auth is None:
-        return
-
-    excluded_paths = ['/api/v1/status/',
-                      '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
-
-    if not auth.require_auth(request.path, excluded_paths):
-        return
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if auth.current_user(request) is None:
-        abort(403)
+    if auth:
+        excluded_paths = [
+            '/api/v1/status/',
+            '/api/v1/unauthorized/',
+            '/api/v1/forbidden/',
+        ]
+        if auth.require_auth(request.path, excluded_paths):
+            auth_header = auth.authorization_header(request)
+            user = auth.current_user(request)
+            if auth_header is None:
+                abort(401)
+            if user is None:
+                abort(403)
 
 
 if __name__ == "__main__":
