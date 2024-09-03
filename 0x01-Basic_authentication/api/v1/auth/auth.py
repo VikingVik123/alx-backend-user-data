ab97fcc6f4008a882a45682425fb9cc2574c8b74
VikingVik123 <vodimmegwa@gmail.com>
2024-09-03 13:52:17 +0000

    Fied pycodestyle err

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,17 +8,17 @@ from typing import List, Union, TypeVar
 
 class Auth:
 
-
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        func 2 check authorization
+        """
         pass
-
 
     def authorization_header(self, request=None) -> str:
         """
         checks for authorization
         """
         return None
-
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
