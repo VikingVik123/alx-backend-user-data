9a5b56e1674bc852fd8cb8ea7ce9796c4f0ac75a
VikingVik123 <114689303+VikingVik123@users.noreply.github.com>
2024-09-04 05:22:57 +0100

    task 9

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -41,3 +41,14 @@ class BasicAuth(Auth):
             return decoded_str
         except (binascii.Error, UnicodeDecodeError):
             return None
+        
+    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> Tuple[str, str]:
+        """
+        method 2 extract creds
+        """
+        if decoded_base64_authorization_header is None or not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        email, password = decoded_base64_authorization_header.split(':', 1)
+        return email, password
