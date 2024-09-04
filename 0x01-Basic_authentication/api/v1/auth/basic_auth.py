8955f7e6a8ddd45a334c5c1235b5ab46f6bc0cd0
VikingVik123 <114689303+VikingVik123@users.noreply.github.com>
2024-09-04 04:34:02 +0100

    task 6

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Basic auth function for the API.
+"""
+import re
+import base64
+import binascii
+from typing import Tuple, TypeVar
+
+from .auth import Auth
+from models.user import User
+
+
+class BasicAuth(Auth):
+    """
+    creates BasicAuth class
+    """
+    
