33cd9ef42d4fb713ef689e8d9ccb6590eb1bed0e
VikingVik123 <114689303+VikingVik123@users.noreply.github.com>
2024-09-04 05:11:09 +0100

    task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -15,4 +15,14 @@ class BasicAuth(Auth):
     """
     creates BasicAuth class
     """
-    
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """
+        method for base64 auth
+        """
+        if authorization_header is None or not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        base64_part = authorization_header[len("Basic "):]
+        return base64_part
