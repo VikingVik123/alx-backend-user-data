4c5ef04b783411983c7688dc2951adfafae0b804
VikingVik123 <114689303+VikingVik123@users.noreply.github.com>
2024-09-05 16:37:09 +0100

    task 3

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -32,5 +32,12 @@ class SessionAuth(Auth):
         self.user_id_by_session_id[session_id] = user_id
         return session_id
 
-
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        if session_id is None:
+            return None
+        if not isinstance(session_id, str):
+            return None
+        
+        user_id = self.user_id_by_session_id.get(session_id)
+        return user_id
 
