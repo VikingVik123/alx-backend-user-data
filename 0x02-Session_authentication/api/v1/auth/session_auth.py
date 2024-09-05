d578b43bbfa7eeb7ee9d5ca9193e615c916a32e7
VikingVik123 <114689303+VikingVik123@users.noreply.github.com>
2024-09-05 15:27:33 +0100

    task 2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -6,6 +6,7 @@ import re
 import base64
 import binascii
 from typing import Tuple, TypeVar, Optional
+from uuid import uuid4
 
 from .auth import Auth
 from models.user import User
@@ -15,4 +16,18 @@ class SessionAuth(Auth):
     """
     session class creation
     """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+        
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
+
+
 
