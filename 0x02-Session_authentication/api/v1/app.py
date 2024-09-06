c6dda18a337d4778127eb1fcb6271ffb668ad6d3
VikingVik123 <114689303+VikingVik123@users.noreply.github.com>
2024-09-06 04:44:16 +0100

    task 5

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -68,6 +68,8 @@ def authenticate_user() -> Optional[str]:
         abort(401)
     if auth.current_user(request) is None:
         return abort(403)
+    if auth.session_cookie(request) is None:
+        abort(401)
 
 
 if __name__ == "__main__":
