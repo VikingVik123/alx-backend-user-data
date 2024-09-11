5804baac4ed9b90b77c662d9c4555830938cf3e9
VikingVik123 <114689303+VikingVik123@users.noreply.github.com>
2024-09-11 14:22:31 +0100

    tried fix

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -64,7 +64,7 @@ class DB:
         user = self.find_user_by(id=user_id)
         for key, value in kwargs.items():
             if not hasattr(user, key):
-                raise ValueError(f"{key} is not an attribute of the User class.")
+                raise ValueError(f"{key} is not an attribute of the User")
 
         setattr(user, key, value)
         self._session.commit()
