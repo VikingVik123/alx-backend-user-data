78e4076d23d139f849ee555f2fdec86aa3c83fe7
VikingVik123 <114689303+VikingVik123@users.noreply.github.com>
2024-09-11 15:42:17 +0100

    tried task 7

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,9 +3,10 @@
 Script to config a bsic flask app
 """
 from flask import Flask
-from flask import jsonify
+from flask import jsonify, request, abort
+from auth import Auth
 
-
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -15,6 +16,21 @@ def task():
     A simple GET route that returns a JSON message
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=['POST'], strict_slashes=False)
+def users():
+    """
+    method to return user credentials
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == '__main__':
