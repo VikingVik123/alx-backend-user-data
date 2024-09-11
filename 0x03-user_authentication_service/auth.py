774affb3db0f9afd4f3d057226ebdcd7af253af1
VikingVik123 <114689303+VikingVik123@users.noreply.github.com>
2024-09-11 12:25:53 +0100

    tried task 5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,8 @@
 method that takes in a password string arguments and returns bytes
 """
 import bcrypt
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> str:
@@ -12,3 +14,21 @@ def _hash_password(password: str) -> str:
     hashed_encoded = password.encode('utf-8')
     hashed = bcrypt.hashpw(hashed_encoded, bcrypt.gensalt())
     return hashed.decode('utf-8')
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        existing_email = self._db.find_user_by(email=email)
+        if email == existing_email:
+            raise ValueError(f"User {email} already exists")
+
+        else:
+            pwd = _hash_password(password)
+            new_user = self._db.add_user(email, pwd)
+            return new_user
